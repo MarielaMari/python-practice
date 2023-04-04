78fa5cc0f07331b638b08127b8be3485ee3a5c40
Mariela <mvmihilova@gmail.com>
2023-04-04 15:09:23 -0600

    added age

--- oop.py
+++ oop.py
@@ -1,15 +1,17 @@
 # OOP
 
 class PlayerCharacter:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
 
     def run(self):
         print('run')
+        return 'done'
 
 
-player1 = PlayerCharacter("Cindy")
-player2 = PlayerCharacter("Tommy")
+player1 = PlayerCharacter("Cindy", 33)
+player2 = PlayerCharacter("Tommy", 26)
 print()
 print(player1.name)
 print(player2.name)
