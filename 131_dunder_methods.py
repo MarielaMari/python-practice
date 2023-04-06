c504bea9d623aed2c26db04e156c1d148b7f30ba
Mariela <mvmihilova@gmail.com>
2023-04-06 16:01:33 -0600

    init

---
+++ 131_dunder_methods.py
@@ -0,0 +1,23 @@
+# Dunder Methods
+# Do not modify
+# Only somethimes, when you really need to, you can do so!
+class Toy():
+    def __init__(self, color, age):
+        self.color = color
+        self.age = age
+
+    def __str__(self):
+        return f'{self.color}'
+
+    def __len__(self):  # sometimes we need to modify a dunder method
+        return 5
+
+    def __call__(self):  # note: __call__ is used under the hood to call a function!
+        return ('yess??')
+
+
+action_figure = Toy('red', 0)
+print(action_figure.__str__())
+print(str(action_figure))
+print(len(action_figure))
+print(action_figure())
