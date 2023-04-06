cfbddf139b4bbd7e46bdcdcd00907862f72aba7a
Mariela <mvmihilova@gmail.com>
2023-04-06 16:08:00 -0600

    str, len, call, getitem

--- 131_dunder_methods.py
+++ 131_dunder_methods.py
@@ -5,6 +5,10 @@ class Toy():
     def __init__(self, color, age):
         self.color = color
         self.age = age
+        self.my_dict = {
+            'name': 'Yoyo',
+            'has_pets': False
+        }
 
     def __str__(self):
         return f'{self.color}'
@@ -15,9 +19,13 @@ class Toy():
     def __call__(self):  # note: __call__ is used under the hood to call a function!
         return ('yess??')
 
+    def __getitem__(self, i):
+        return self.my_dict[i]
+
 
 action_figure = Toy('red', 0)
 print(action_figure.__str__())
 print(str(action_figure))
 print(len(action_figure))
 print(action_figure())
+print(action_figure['name'])
