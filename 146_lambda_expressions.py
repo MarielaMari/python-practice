576e73ae2897860e78381a0c8a9d861d111ef9c1
Mariela <mvmihilova@gmail.com>
2023-04-08 05:51:02 -0600

    added filter and reduce

--- 146_lambda_expressions.py
+++ 146_lambda_expressions.py
@@ -12,14 +12,17 @@ my_list = [1, 2, 3]
 #     return item*2
 
 
-def only_odd(item):
-    return item % 2 != 0
+# IF WANT TO USE LAMBDA FILTER
+# def only_odd(item):
+#     return item % 2 != 0
 
-
-def accumulator(acc, item):
-    print(acc, item)
-    return acc + item
+# TO USE LAMBDA WITH REDUCE()
+# def accumulator(acc, item):
+#     print(acc, item)
+#     return acc + item
 
 
 print(list(map(lambda item: item*2, my_list)))
+print(list(filter(lambda item: item % 2 != 0, my_list)))
+print(reduce(lambda acc, item: acc+item, my_list))
 print(my_list)
