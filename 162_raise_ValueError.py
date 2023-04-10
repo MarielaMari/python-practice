550e55bf1497d76f52e35231bcaca61417a166a8
Mariela <mvmihilova@gmail.com>
2023-04-10 07:13:52 -0600

    init

---
+++ 162_raise_ValueError.py
@@ -0,0 +1,23 @@
+# Error Handling:
+
+while True:  # need to use a loop, otherwise the code will stop and user will not be able to enter a number
+    try:
+        # note: if I don't use except, the int(input('string')) would give me an error because, I am trying to convert a string to an int!
+        age = int(input('what is your age?'))
+        10/age
+        # useful if you want to let the user know
+        raise ValueError('HEY, CUT IT OUT!')
+    except ValueError:
+        print('please, enter a number')
+        continue
+    except ZeroDivisionError:
+        print('please, enter age higher than 0')
+        break
+    else:
+        print('Thank you!')
+        break  # need to add break to stop running the loop after a number is added!
+
+    finally:
+        # finally runs at the end no matter what! Useful for example in
+        print('ok, I am finally done')
+    print('can you hear me?')
---
+++ 163_generators.py
@@ -0,0 +1,16 @@
+#Generators allow us to create a sequences of values over time.
+
+#range is a generator! Special word used : kill!
+
+
+#range(100)
+#list(range(100))
+
+#def make_list(num):
+#   result = []
+#   for i in range(num):
+#   result.append(i*2)
+#   return result
+
+#my_list = make_list(100)
+#print(my_list)                         #my_list is taking up a lot of memory
